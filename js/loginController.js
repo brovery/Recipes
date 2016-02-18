ea6af4822d0a0a98c489a6b90df8b023f38fa053
danielgraff <ddgraff12@gmail.com>
2016-02-18 09:40:40 -0700

    more login
    Create user, change email, change password added. Also updated to new
    firebase url

--- js/loginController.js
+++ js/loginController.js
@@ -1,71 +1,128 @@
 /**
  * Created by danielgraff1 on 2/17/16.
  */
-(function(){
+(function () {
     'use strict';
 
     angular.module('loginController', [])
         .controller('loginController', loginController);
 
-loginController.$inject = ['$timeout'];
+    loginController.$inject = ['$timeout'];
 
-function loginController($timeout) {
+    function loginController($timeout) {
 
-    // controller data and functions
-    var lc = this;
-    lc.email = "";
-    lc.password = "";
-    lc.googleLogin = googleLogin;
-    lc.deleteGoogleData = deleteGoogleData;
-    lc.nativeLogin = nativeLogin;
+        // controller data and functions
+        var lc = this;
+        lc.email = "";
+        lc.password = "";
+        lc.createEmail = "";
+        lc.createPassword = "";
+        lc.oldEmail = "";
+        lc.oldPassword = "";
+        lc.newEmail = "";
+        lc.newPassword = "";
+        lc.googleLogin = googleLogin;
+        lc.deleteGoogleData = deleteGoogleData;
+        lc.nativeLogin = nativeLogin;
+        lc.create = create;
+        lc.changeEmail = changeEmail;
+        lc.changePassword = changePassword;
 
-    lc.gData = 'firebase:session::recipe-app.firebaseio.com';
-    // if google data is found in local storage, use it
-    lc.message = lc.gData && lc.gData.google ? "Logged in to Google." : "No Google data found.";
+        lc.gData = 'firebase:session::geo-recipes.firebaseio.com';
+        // if google data is found in local storage, use it
+        lc.message = lc.gData && lc.gData.google ? "Logged in to Google." : "No Google data found.";
 
-    // IMPORTANT: change to match the URL of your Firebase.
-    var url = 'https://recipe-app.firebaseio.com';
+        // IMPORTANT: change to match the URL of your Firebase.
+        var url = 'https://geo-recipes.firebaseio.com/';
 
-    // use Firebase library to login to google
-    function googleLogin() {
-        var ref = new Firebase(url);
-        ref.authWithOAuthPopup('google', function (error, authData) {
-            if (error) {
-                console.log('Log in to Google Failed', error);
-                lc.message = 'Log in to Google Failed. ' + error;
-            } else {
-                console.log('Logged in to Google');
-                lc.message = 'Logged in to Google.';
-                $timeout(function () { // invokes $scope.$apply()
-                    lc.gData = authData;
+//Google
+        // use Firebase library to login to google
+        function googleLogin() {
+            var ref = new Firebase(url);
+            ref.authWithOAuthPopup('google', function (error, authData) {
+                if (error) {
+                    console.log('Log in to Google Failed', error);
+                    lc.message = 'Log in to Google Failed. ' + error;
+                } else {
+                    console.log('Logged in to Google');
+                    lc.message = 'Logged in to Google.';
+                    $timeout(function () { // invokes $scope.$apply()
+                        lc.gData = authData;
+                    });
+                }
+            });
+        }
+
+        // this removes google data from local storage
+        // to FULLY logout, you MUST go to google.com and logout
+        function deleteGoogleData() {
+            lc.gData = {};
+            lc.message = 'google data deleted.';
+        }
+
+//Native login
+        function nativeLogin() {
+            console.log('logging in');
+            var ref = new Firebase(url);
+            if (lc.email !== "" || lc.password !== "") {
+
+
+                ref.authWithPassword({
+                    email: lc.email,
+                    password: lc.password
+                }, function (error, authData) {
+                    console.log(error + authData);
+                }, {
+                    remember: "sessionOnly"
                 });
             }
-        });
+        }
+
+//Create native user - https://www.firebase.com/docs/web/guide/login/password.html
+        function create() {
+            var ref = new Firebase(url);
+            ref.createUser({
+                email: lc.createEmail,
+                password: lc.createPassword
+            }, function (error, userData) {
+                if (error) {
+                    console.log("Error creating user:", error);
+                } else {
+                    console.log("Successfully created user account with uid:", userData.uid);
+                }
+            });
+        }
+
+//Change email - https://www.firebase.com/docs/web/guide/login/password.html
+        function changeEmail() {
+            var ref = new Firebase(url);
+            ref.changeEmail({
+                oldEmail : lc.oldEmail,
+                newEmail : lc.newEmail,
+                password : lc.password
+            }, function(error) {
+                if (error === null) {
+                    console.log("Email changed successfully");
+                } else {
+                    console.log("Error changing email:", error);
+                }
+            });
+        }
+
+//Change password - https://www.firebase.com/docs/web/guide/login/password.html
+        function changePassword() {
+            var ref = new Firebase(url);
+            ref.changePassword({
+                email       : lc.email,
+                oldPassword : lc.oldPassword,
+                newPassword : lc.newPassword,
+            }, function(error) {
+                if (error === null) {
+                    console.log("Password changed successfully");
+                } else {
+                    console.log("Error changing password:", error);
+                }
+            });
+        }
     }
-
-    // this removes google data from local storage
-    // to FULLY logout, you MUST go to google.com and logout
-    function deleteGoogleData() {
-        lc.gData = {};
-        lc.message = 'google data deleted.';
-    }
-
-    function nativeLogin() {
-        console.log('logging in');
-        var ref = new Firebase(url);
-        if(lc.email !== "" || lc.password !== ""){
-
-
-            ref.authWithPassword({
-            email: lc.email,
-            password: lc.password
-        }, function(error, authData) { console.log(error + authData); }, {
-            remember: "sessionOnly"
-        });
-    }}
-}
-
-    // bug alert: this delete function sometimes does NOT reset the local storage,
-    // so a page refresh finds google data in localstorage.
-
 }());
