127f8dfbae21b2d95307b9bf9884ffdd708bdd8e
danielgraff <ddgraff12@gmail.com>
2016-02-22 10:07:46 -0700

    Login modal
    Still need to clear modal on hide

--- js/loginController.js
+++ js/loginController.js
@@ -21,10 +21,13 @@
         lc.oldPassword = "";
         lc.newEmail = "";
         lc.newPassword = "";
+        lc.loginHide = false;
+        lc.successHide = false;
+        lc.failHide = false;
         lc.googleLogin = googleLogin;
         lc.deleteGoogleData = deleteGoogleData;
         lc.githubLogin = githubLogin;
-        lc.deletegithubData = deletegithubData;
+        lc.deleteGithubData = deleteGithubData;
         lc.nativeLogin = nativeLogin;
         lc.create = create;
         lc.changeEmail = changeEmail;
@@ -48,6 +51,8 @@
                 } else {
                     console.log('Logged in to Google');
                     lc.message = 'Logged in to Google.';
+                    lc.loginHide = true;
+                    lc.loginHideGoogle = true;
                     $timeout(function () { // invokes $scope.$apply()
                         lc.gData = authData;
                     });
@@ -73,6 +78,8 @@
                 } else {
                     console.log('Logged in to github');
                     lc.message = 'Logged in to github.';
+                    lc.loginHide = true;
+                    lc.loginHideGithub = true;
                     $timeout(function () { // invokes $scope.$apply()
                         lc.ghData = authData;
                     });
@@ -82,7 +89,7 @@
 
         // this removes github data from local storage
         // to FULLY logout, you MUST go to github.com and logout
-        function deletegithubData() {
+        function deleteGithubData() {
             lc.ghData = {};
             lc.message = 'github data deleted.';
         }
@@ -99,8 +106,17 @@
                     password: lc.password
                 }, function (error, authData) {
                     console.log(error + authData);
+                    if (error !== null) {
+                        var wrong = "Bad username or Password";
+                    } else {
+                        $timeout(function () {
+                            lc.loginHide = true;
+                            lc.loginHideNative = true;
+                        });
+                    }
                 }, {
                     remember: "sessionOnly"
+
                 });
             }
         }
@@ -112,11 +128,16 @@
                 email: lc.createEmail,
                 password: lc.createPassword
             }, function (error, userData) {
-                if (error) {
-                    console.log("Error creating user:", error);
-                } else {
-                    console.log("Successfully created user account with uid:", userData.uid);
-                }
+                $timeout(function () {
+                    if (error) {
+                        console.log("Error creating user:", error);
+                        lc.failHide = true;
+                        lc.failLoginHide = true;
+                    } else {
+                        console.log("Successfully created user account with uid:", userData.uid);
+                        lc.successHide = true;
+                    }
+                });
             });
         }
 
@@ -124,15 +145,21 @@
         function changeEmail() {
             var ref = new Firebase(url);
             ref.changeEmail({
-                oldEmail : lc.oldEmail,
-                newEmail : lc.newEmail,
-                password : lc.password
-            }, function(error) {
-                if (error === null) {
-                    console.log("Email changed successfully");
-                } else {
-                    console.log("Error changing email:", error);
-                }
+                oldEmail: lc.oldEmail,
+                newEmail: lc.newEmail,
+                password: lc.password
+            }, function (error) {
+                $timeout(function () {
+                    if (error === null) {
+                        console.log("Email changed successfully");
+                        lc.successHide = true;
+
+                    } else {
+                        console.log("Error changing email:", error);
+                        lc.failHide = true;
+                        lc.emailFail = true;
+                    }
+                });
             });
         }
 
@@ -140,16 +167,26 @@
         function changePassword() {
             var ref = new Firebase(url);
             ref.changePassword({
-                email       : lc.email,
-                oldPassword : lc.oldPassword,
-                newPassword : lc.newPassword,
-            }, function(error) {
-                if (error === null) {
-                    console.log("Password changed successfully");
-                } else {
-                    console.log("Error changing password:", error);
-                }
+                email: lc.email,
+                oldPassword: lc.oldPassword,
+                newPassword: lc.newPassword,
+            }, function (error) {
+                $timeout(function () {
+                    if (error === null) {
+                        console.log("Password changed successfully");
+                        lc.successHide = true;
+                    } else {
+                        console.log("Error changing password:", error);
+                        lc.failHide = true;
+                        lc.passFail = true;
+                    }
+                });
             });
+        }
+
+//clear modal on close
+        function clearModal () {
+            
         }
     }
 }());
